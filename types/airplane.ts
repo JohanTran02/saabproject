3fa8a9a0e88dabf81c71ebcb832404ae253cb88a
JohanTran <johan.tran0210@gmail.com>
2026-03-16 14:32:30 +0100

    feat(types): add types for objects

--- types/airplane.ts
+++ types/airplane.ts
@@ -2,7 +2,7 @@ import { Resource } from "./resources";
 
 export type Airplane = {
     id: string;
-    weapons: Extract<Resource, { type: 'ammunition' }>[];
+    // weapons: Extract<Resource, { type: 'ammunition' }>[];
     ammunition: Extract<Resource, { type: 'ammunition' }>[];
     fuel: Extract<Resource, { type: 'fuel' }>;
     battery: Extract<Resource, { type: 'battery' }>;
