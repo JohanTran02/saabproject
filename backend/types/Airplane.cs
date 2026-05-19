8ba163c18849d0247a7c85baf702b971d7d64407
JohanTran <johan.tran0210@gmail.com>
2026-05-19 20:05:48 +0200

    refactor(airplane):initialize properties with default values

--- backend/types/Airplane.cs
+++ backend/types/Airplane.cs
@@ -3,9 +3,8 @@ namespace backend.types
     public class Airplane
     {
         public int Id { get; set; }
-        public List<Ammunition>? Ammunition { get; set; }
-        public List<Fuel>? Fuel { get; set; }
-        public List<Battery>? Battery { get; set; }
+        public List<Ammunition> Ammunition { get; set; } = [];
+        public List<Fuel> Fuel { get; set; } = [];
+        public List<Battery> Battery { get; set; } = [];
     }
-
 }
