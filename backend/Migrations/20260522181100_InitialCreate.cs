96d39e23c01216fc951f9b86480b9d2382636bfc
JohanTran <johan.tran0210@gmail.com>
2026-05-23 15:18:42 +0200

    feat(database):fix database connection and migration

---
+++ backend/Migrations/20260522181100_InitialCreate.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airplanes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airplanes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderTitle = table.Column<string>(type: "text", nullable: false),
+                    Comments = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StartedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceBuffer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MaxReqAmount = table.Column<int>(type: "integer", nullable: false),
+                    OptimalReqAmount = table.Column<int>(type: "integer", nullable: false),
+                    MinReqAmount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceBuffer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Sku = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Ammunition_AirplaneId = table.Column<int>(type: "integer", nullable: true),
+                    Battery_AirplaneId = table.Column<int>(type: "integer", nullable: true),
+                    AirplaneId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resources_Airplanes_AirplaneId",
+                        column: x => x.AirplaneId,
+                        principalTable: "Airplanes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Resources_Airplanes_Ammunition_AirplaneId",
+                        column: x => x.Ammunition_AirplaneId,
+                        principalTable: "Airplanes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Resources_Airplanes_Battery_AirplaneId",
+                        column: x => x.Battery_AirplaneId,
+                        principalTable: "Airplanes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderId = table.Column<int>(type: "integer", nullable: false),
+                    MaintenanceOrderId = table.Column<int>(type: "integer", nullable: false),
+                    AirplaneId = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Deadline = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Comments = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StartedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Airplanes_AirplaneId",
+                        column: x => x.AirplaneId,
+                        principalTable: "Airplanes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Orders_MaintenanceOrderId",
+                        column: x => x.MaintenanceOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personnel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    MaintenanceTaskId = table.Column<int>(type: "integer", nullable: true),
+                    CurrentOrderId = table.Column<int>(type: "integer", nullable: true),
+                    CurrentTaskId = table.Column<int>(type: "integer", nullable: true),
+                    Technician_CurrentTaskId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personnel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Personnel_Orders_CurrentOrderId",
+                        column: x => x.CurrentOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Personnel_Tasks_CurrentTaskId",
+                        column: x => x.CurrentTaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Personnel_Tasks_MaintenanceTaskId",
+                        column: x => x.MaintenanceTaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Personnel_Tasks_Technician_CurrentTaskId",
+                        column: x => x.Technician_CurrentTaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceRequirements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TaskId = table.Column<int>(type: "integer", nullable: false),
+                    ResourceId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    BufferId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceRequirements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResourceRequirements_ResourceBuffer_BufferId",
+                        column: x => x.BufferId,
+                        principalTable: "ResourceBuffer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResourceRequirements_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResourceRequirements_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CurrentTaskId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sites_Tasks_CurrentTaskId",
+                        column: x => x.CurrentTaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personnel_CurrentOrderId",
+                table: "Personnel",
+                column: "CurrentOrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personnel_CurrentTaskId",
+                table: "Personnel",
+                column: "CurrentTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personnel_MaintenanceTaskId",
+                table: "Personnel",
+                column: "MaintenanceTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personnel_Technician_CurrentTaskId",
+                table: "Personnel",
+                column: "Technician_CurrentTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceRequirements_BufferId",
+                table: "ResourceRequirements",
+                column: "BufferId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceRequirements_ResourceId",
+                table: "ResourceRequirements",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceRequirements_TaskId",
+                table: "ResourceRequirements",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_AirplaneId",
+                table: "Resources",
+                column: "AirplaneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_Ammunition_AirplaneId",
+                table: "Resources",
+                column: "Ammunition_AirplaneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_Battery_AirplaneId",
+                table: "Resources",
+                column: "Battery_AirplaneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sites_CurrentTaskId",
+                table: "Sites",
+                column: "CurrentTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_AirplaneId",
+                table: "Tasks",
+                column: "AirplaneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_MaintenanceOrderId",
+                table: "Tasks",
+                column: "MaintenanceOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personnel");
+
+            migrationBuilder.DropTable(
+                name: "ResourceRequirements");
+
+            migrationBuilder.DropTable(
+                name: "Sites");
+
+            migrationBuilder.DropTable(
+                name: "ResourceBuffer");
+
+            migrationBuilder.DropTable(
+                name: "Resources");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Airplanes");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
