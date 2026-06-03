e2c2f30a06e503947b1ee3a08c9d94230ad1f821
JohanTran <johan.tran0210@gmail.com>
2026-06-03 10:36:22 +0200

    feat(resource):add unique indexes for resources

---
+++ backend/Migrations/20260603083555_AddUniqueResourceRestraints.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueResourceRestraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_Name_Type",
+                table: "Resources",
+                columns: new[] { "Name", "Type" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_Sku",
+                table: "Resources",
+                column: "Sku",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Resources_Name_Type",
+                table: "Resources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resources_Sku",
+                table: "Resources");
+        }
+    }
+}
